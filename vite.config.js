7a374fda34a8777c6510e01c77100aef8919c997
aleks <achudars@gmail.com>
2025-08-03 17:29:14 +0100

    chore: add reporters configuration to test setup in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,13 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/test/setup.jsx"],
+    reporters: [
+      [
+        "default",
+        {
+          summary: false,
+        },
+      ],
+    ],
   },
 });
